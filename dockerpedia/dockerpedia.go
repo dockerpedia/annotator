e4a9dcad20cac06cfaed018a0bc79948390afa3e
Maximiliano Osorio <maxiosorio@gmail.com>
2018-06-26 08:56:32 -0400

    Get the size

--- dockerpedia/dockerpedia.go
+++ dockerpedia/dockerpedia.go
@@ -6,12 +6,13 @@ import (
 
 	"github.com/dockerpedia/annotator/clair"
 	"github.com/dockerpedia/annotator/klar"
+	"github.com/dockerpedia/docker-registry-client/registry"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Tag struct {
-	Name     string           `form:"tag" json:"tag" binding:"required"`
+	Tag      string           `form:"tag" json:"tag" binding:"required"`
 	Image    string           `form:"image" json:"image" binding:"required"`
 	Size     int64            `json:"size"`
 	Features []*clair.Feature `json:"features"`
@@ -22,16 +23,15 @@ var username string = "" // anonymous
 var password string = "" // anonymous
 
 func NewRepository(c *gin.Context) {
-
-	//	hub := registry.New(url, username, password)
+	hub := registry.New(url, username, password)
 	var json Tag
 	if err := c.ShouldBindJSON(&json); err == nil {
-		//size, err := hub.TagSize(json.Repository, json.Image)
-		//json.Size = size
+		size, err := hub.TagSize(json.Image, json.Tag)
+		json.Size = size
 		json.Features = klar.Run(json.Image)
 
 		if err != nil {
-			log.Fatal("Unable to the get size")
+			log.Printf("Unable to the get size of the image %s:%s", json.Image, json.Tag)
 		}
 		c.JSON(http.StatusOK, gin.H{
 			"result": json,
