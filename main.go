dd4f49dff1043b2183d3cd6d4e889f4c83e869f6
Maximiliano Osorio <maxiosorio@gmail.com>
2018-08-10 16:23:08 -0400

    added static web server (naive approach)

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"github.com/dockerpedia/annotator/dockerpedia"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	"net/http"
 )
 
 func main() {
@@ -12,8 +13,11 @@ func main() {
 
 	v1 := router.Group("/api/v1/")
 	{
-		v1.POST("/repositories/new", dockerpedia. NewRepository)
+		v1.POST("/repositories/new", dockerpedia.NewRepository)
+
 	}
+	router.StaticFS("/workflows/", http.Dir("workflows/"))
+
 
 	router.Run(":8081")
 }
