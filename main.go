0590db6dbf64fb87cffd18465ed29fa42e5449b2
Maximiliano Osorio <maxiosorio@gmail.com>
2019-05-18 15:56:41 -0400

    Add: configuration

--- main.go
+++ main.go
@@ -1,46 +1,21 @@
 package main
 
 import (
+	"github.com/dockerpedia/annotator/internal/config"
 	"github.com/dockerpedia/annotator/dockerpedia"
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
+	"log"
 	"net/http"
-	"github.com/spf13/viper"
-	"fmt"
-	"os"
+
 )
 
-
-type ServerConfig struct {
-	Listen string
-	Port string
-}
-
-type EndpointConfig struct {
-	Server string `mapstructure:"address"`
-}
-
-type Config struct {
-	Endpoint  EndpointConfig `mapstructure:"endpoint"`
-	Server ServerConfig   `mapstructure:"server"`
-}
-
-
 func main() {
-	v := viper.New()
-	v.SetConfigName("config")
-	v.AddConfigPath(".")
-	if err := v.ReadInConfig(); err != nil {
-		fmt.Printf("couldn't load config: %s", err)
-		os.Exit(1)
-	}
-	var c Config
-	if err := v.Unmarshal(&c); err != nil {
-		fmt.Printf("couldn't read config: %s", err)
-	}
-
-
 	router := gin.Default()
+	configuration, err := config.New()
+	if err != nil {
+		log.Panicln("Configuration error", err)
+	}
 
 	v1 := router.Group("/api/v1/")
 	{
@@ -52,5 +27,6 @@ func main() {
 
 
 
-	router.Run(c.Server.Listen)
+	router.Run(configuration.Server.Listen)
 }
+
